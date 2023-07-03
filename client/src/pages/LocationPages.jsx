af52dd8dc5162682f2708dd82d957e7b6d8fdd88
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-03 12:52:17 +0100

    updating accommodation form

--- client/src/pages/LocationPages.jsx
+++ client/src/pages/LocationPages.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { ImEyePlus } from 'react-icons/im'
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { ImEyePlus } from 'react-icons/im';
+import { SlCloudUpload } from 'react-icons/sl';
 
 const LocationPages = () => {
     const { id } = useParams();
@@ -15,7 +16,21 @@ const LocationPages = () => {
              { id == 'new' && (
                     <form>
                         <label htmlFor="title">Title:</label>
-                        <input type="text"  />
+                        <input type="text" placeholder='Enter a New Place'/>
+                        <label htmlFor="address">Address:</label>
+                        <input type="text" placeholder='New address'/>
+                        <label htmlFor="photo">Photos:</label>
+                        <div className='flex'>
+                            <input type="text" placeholder='Add using a link'/>
+                            <button className='bg-primary'>Add&nbsp;Photo</button>
+                        </div>
+                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                            <button className='bg-transparent flex items-center gap-2 border border-gray-200 rounded-lg text-xl p-2'><SlCloudUpload/>Upload</button>
+                        </div>
+                        <label htmlFor="description">Description:</label>
+                        <textarea name="" id="" cols="30" rows="10" placeholder='Enter your description...'></textarea>
+                        <label htmlFor="perks">Perks:</label>
+                        
                     </form>
                 )
              }
