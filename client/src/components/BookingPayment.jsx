f3cbd062f992db9d0168ebc27f8775db8ccc61a9
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-28 09:49:10 +0100

    airbnb booking app

--- client/src/components/BookingPayment.jsx
+++ client/src/components/BookingPayment.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { differenceInCalendarDays, format } from 'date-fns';
 import { FcCalendar } from 'react-icons/fc';
+import { MdModeNight } from 'react-icons/md';
 
 const BookingPayment = ({ booking }) => {
   // Custom format string: 'd EEE,MMMM yyyy'
@@ -11,21 +12,22 @@ const BookingPayment = ({ booking }) => {
     <>
       <div className='bg-gray-200 text-black p-3 rounded-xl'>
         <div className=' flex justify-between gap-2 border-t border-white-300 mt-2 py-2'>
-            <div>
+            <div className='flex flex-col gap-1'>
           <div >
-            <h1>Check-In Date:</h1>
+            <h1 className='underline  px-2 rounded-sm text-black my-1'>Check-In Date:</h1>
             <div className='flex gap-2 items-center'>
             <FcCalendar /> {formattedCheckInDate} 
             </div>
           </div>
           <div >
-          <h1>Check-Out Date:</h1>
+          <h1 className='underline px-2 rounded-sm text-black my-1'>Check-Out Date:</h1>
           <div className='flex gap-2 items-center'>
             <FcCalendar /> {formattedCheckOutDate}
           </div>
           </div>
            <div >
-            <h1>Number of Nights:</h1>
+            <h1 className='underline px-2 rounded-sm text-black my-1'>Number of Nights:</h1>
+            <MdModeNight/>
           <p>{differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} night(s)</p> 
           {/* Price: ${booking.price} */}
         </div>
